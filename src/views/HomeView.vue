b4a166fe5c01e8dd1507c4bf02f7023e21e86852
jeroen-engineer <jeroentheunissen@protonmail.com>
2022-05-08 13:17:20 +0200

    install tailwind and cleaned up starter

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1 class="text-blue-500 text-3xl">Home page</h1>
   </main>
 </template>
